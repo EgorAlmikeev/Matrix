3ca1b017b91fd05843ecf5cd27efe62c12837075
York <york.git@gmail.com>
2017-10-30 19:18:17 +0300

    samples fixed

--- Sources/Sample Projects/QtCreator Sample projects/Equations System Solution Sample [Reverse Method]/ReverseMethod/main.cpp
+++ Sources/Sample Projects/QtCreator Sample projects/Equations System Solution Sample [Reverse Method]/ReverseMethod/main.cpp
@@ -28,12 +28,12 @@ int main()
     {
 
         cout << "\n\n\tleft part of eqations system setup : " << endl;
-        matrix_a.SetElements();
-        matrix_a.Show();
+        matrix_a.setElements();
+        matrix_a.show();
 
-        cout << "\n\n\tdeterminant : " << matrix_a.GetDeterminant();
+        cout << "\n\n\tdeterminant : " << matrix_a.getDeterminant();
 
-        if(matrix_a.GetDeterminant() == 0)
+        if(matrix_a.getDeterminant() == 0)
         {
             cout << "\n\tsystem has no solution...";
             cout << "\n\n\tEnd of program\n";
@@ -41,32 +41,32 @@ int main()
         }
 
         cout << "\n\n\tadjoint matrix : ";
-        matrix_a.GetAdjoint().Show();
+        matrix_a.getAdjoint().show();
         cout << "\n\n\treverse matrix : ";
-        matrix_a.GetReverse().Show();
+        matrix_a.getReverse().show();
         cout << "\n\n\tright part of equations system setup : " << endl;
-        matrix_b.SetElements();
+        matrix_b.setElements();
         cout << "\n\n\tX = A^(-1) * B = ";
-        matrix_x = matrix_a.GetReverse().Multiplicate(matrix_b);
-        matrix_x.Show();
+        matrix_x = matrix_a.getReverse().multiplicate(matrix_b);
+        matrix_x.show();
 
         cout << endl;
 
         for(int i = 1; i <= rows; ++i)
-            cout << "\n\tx" << i << " = " << matrix_x.EditElement(i, 1);
+            cout << "\n\tx" << i << " = " << matrix_x.at(i, 1);
 
     }
     catch(Matrix<MatrixType>::MatrixArithmeticException exp)
     {
-        exp.errmsg();
+        exp.print_message();
     }
     catch(Matrix<MatrixType>::MatrixAccessException exp)
     {
-        exp.errmsg();
+        exp.print_message();
     }
     catch(Matrix<MatrixType>::MatrixTypeException exp)
     {
-        exp.errmsg();
+        exp.print_message();
     }
 
     cout << "\n\n\tEnd of program\n";
